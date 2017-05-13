35772a4cfcc8132661149196efe222e8a8c04a2c
alamote <sergunie@gmail.com>
2017-05-14 01:15:31 +0300

    update

--- input.php
+++ input.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,49 +11,79 @@
 
 </header>
 
-<form>
-    <div id="block">
-        <table>
-            <tr>
-                <td>
-                    <label for="var_count">Кількість змінних</label>
-                </td>
-                <td>
-                    <input autofocus type="number" name="var_count" id="var_count" min="1">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="lim_count">Кількість обмежень</label>
-                </td>
-                <td>
-                    <input type="number" name="lim_counte" id="lim_count" min="1">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="func">Функція</label>
-                </td>
-                <td>
-                    <select name="func" id="func">
-                        <option>max</option>
-                        <option>min</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" id="button_td">
-                    <input type="button" id="calc" value="Підтвердити">
-                </td>
-            </tr>
-        </table>
-    </div>
+<p>Введіть коефіцієнти</p>
+<table id="input_table">
+    <?php
+    $var_count = intval($_GET["var_count"]);
+    $lim_count = intval($_GET["lim_count"]);
+    $func = intval($_GET["func"]);
 
-</form>
-<footer>
-    <span class="left">Бас Євгеній &copy; 2017</span><span class="right">
-    <a href="https://vk.com/id189464921"><img src="image/vk.png" alt="Профіль ВК"  title="Профіль ВК"></a></span>
-</footer>
-<script src="script.js"></script>
+    echo "<tr><td>F<sub>" . $_GET["func"] . "</sub> = </td>";
+    for ($i = 0; $i < $var_count; $i++) {
+        echo "<td><input type='text' class='coef' id='x" . ($i + 1) . "'> x<sub>" . ($i + 1) . "</sub><sup> </sup></td>";
+        if ($i != $var_count - 1) {
+            echo "<td> + </td>";
+        }
+    }
+    for ($i = 0; $i < $var_count; $i++) {
+        echo "<td><input type='text' class='coef' id='x" . ($i + 1) . "_2'> x<sup>2</sup><sub>" . ($i + 1) . "</sub></td>";
+        if ($i != $var_count - 1) {
+            echo "<td> + </td>";
+        }
+    }
+    for ($i = 1; $i <= $var_count - 1; $i++) {
+        $sec_index = $i + 1;
+        for ($j = $var_count - $i; $j > 0; $j--) {
+            echo "<td><input type='text' class='coef' id='x" . ($i) . "x'.$sec_index> x<sub>" . ($i) . "</sub>x<sub>" . ($sec_index++) . "</sub><sup> </sup></td>";
+            if ($j != 1) {
+                echo "<td> + </td>";
+            }
+        }
+        if ($i != $var_count - 1)
+            echo "<td> + </td>";
+    }
+
+    echo "<td> + <input type='text' class='coef' id='const'><sup> </sup></td></tr></table>";
+    echo "<p>Введіть обмеження</p>";
+    echo "<table id='lim_table'>";
+
+    for ($i = 0; $i < $lim_count; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < $var_count; $j++) {
+            echo "<td><input type='text' class='coef' id='lim_".($i+1)."_x".($j+1)."'> x<sub>".($j+1)."</sub></td>";
+            if ($j != $var_count - 1) {
+                echo "<td> + </td>";
+            }
+        }
+        echo "
+            <td><select>
+                <option>≤</option>
+                <option>=</option>
+                <option>≥</option>
+            </select></td>";
+        echo "<td><input type='text' class='coef' id='lim_".($i+1)."'></td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+
+    function get_sum($n)
+    {
+        $sum = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $sum += $i;
+        }
+        return $sum;
+    }
+
+    ?>
+
+
+    <footer>
+        <span class="left">Бас Євгеній &copy; 2017</span><span class="right">
+    <a href="https://vk.com/id189464921"><img src="image/vk.png" alt="Профіль ВК" title="Профіль ВК"></a></span>
+    </footer>
+    <script src="script.js"></script>
 </body>
 </html>
